9adf43264ad45e8f95755e3e106ad53ab0da1d98
matthin <me@justinharrison.ca>
2021-01-27 22:04:17 -0500

    Create navbar

--- frontend/src/Main.js
+++ frontend/src/Main.js
@@ -1,17 +1,22 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 
+import Navbar from './components/Navbar';
 import { Home } from './pages/Home';
 import { Videos } from './pages/Videos';
 import Video from './pages/Video';
 
 function Main() {
   return (
-    <Switch>
-      <Route exact path='/' component={Home}></Route>
-      <Route exact path='/videos' component={Videos}></Route>
-      <Route path='/video/:id' component={Video}></Route>
-    </Switch>
+    <div>
+      <Navbar />
+
+      <Switch>
+        <Route exact path='/' component={Home}></Route>
+        <Route exact path='/videos' component={Videos}></Route>
+        <Route path='/video/:id' component={Video}></Route>
+      </Switch>
+    </div>
   );
 }
 
