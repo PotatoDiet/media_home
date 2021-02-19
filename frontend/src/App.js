262d25c8e910f4bdc86266b905d43a434af4e35a
matthin <me@justinharrison.ca>
2021-02-18 23:33:05 -0500

    Move Main.js into App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,34 +1,23 @@
-import React from 'react';
-import './App.css';
-import Main from './Main.js';
+import React from "react";
+import { Switch, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import { Home } from "./pages/Home";
+import { Videos } from "./pages/Videos";
+import Video from "./pages/Video";
+import "./App.css";
 
 function App() {
-  /*return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );*/
-
-  //return (
-  //  videos.map(video => <Video {...video} />)
-  //)
-
-  return (
+ return (
     <div>
-      <Main />
+      <Navbar />
+
+      <div className="content">
+        <Switch>
+          <Route exact path='/' component={Home}></Route>
+          <Route path='/videos' component={Videos}></Route>
+          <Route path='/video/:id' component={Video}></Route>
+        </Switch>
+      </div>
     </div>
   );
 }
