fab75154ec4af7ba478c10e2ad83d78275bde02a
potatodiet <me@potatodiet.ca>
2022-12-18 19:27:13 -0500

    Have consistent sidebar for normal and admin views

--- CinemaCentral/ClientApp/src/App.tsx
+++ CinemaCentral/ClientApp/src/App.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import {Route, Routes, useNavigate, useRouteError} from 'react-router-dom';
+import {Route, Routes} from 'react-router-dom';
 import Home from './pages/Home';
 import Movies from './pages/Movies';
 import Movie from './pages/Movie';
@@ -7,29 +7,26 @@ import './App.css';
 import Series from "./pages/Series";
 import Episode from "./pages/Episode";
 import Login from "./pages/Login";
-import ContentLayout from "./components/ContentLayout";
-import AdminLayout from "./components/AdminLayout";
 import AdminUsers from "./pages/AdminUsers";
 import {NewUser} from "./pages/NewUser";
 import AdminLibraries from "./pages/AdminLibraries";
+import Layout from "./components/Layout";
 
 export default function App() {
     return (
         <Routes>
-            <Route path="/login" element={<Login />} />
-            
-            <Route path="/admin" element={<AdminLayout />}>
-                <Route path="users" element={<AdminUsers />}/>
-                <Route path="users/new" element={<NewUser />}/>
-                <Route path="libraries" element={<AdminLibraries />}/>
-            </Route>
-    
-            <Route path="/" element={<ContentLayout />}>
+            <Route path="/" element={<Layout />}>
                 <Route index element={<Home/>}/>
+                
+                <Route path="/admin/users" element={<AdminUsers />}/>
+                <Route path="/admin/users/new" element={<NewUser />}/>
+                <Route path="/admin/libraries" element={<AdminLibraries />}/>
+        
                 <Route path="movies" element={<Movies/>}/>
                 <Route path="movie/:id" element={<Movie/>}/>
                 <Route path="series/:id" element={<Series />}/>
                 <Route path="episode/:id" element={<Episode />}/>
+                <Route path="/login" element={<Login />} />
             </Route>
         </Routes>
     )
---
+++ CinemaCentral/ClientApp/src/components/Layout.tsx
@@ -0,0 +1,93 @@
+import React, {useCallback, useEffect, useState} from "react";
+import {Link, Outlet, useLocation, useNavigate} from "react-router-dom";
+import {useQuery} from "react-query";
+import {ccFetch} from "../utitilies";
+
+type User = {
+    role: "Normal" | "Admin"
+}
+
+function SidebarEntry(props: any) {
+    const location = useLocation();
+    const onPage = location.pathname === props.to;
+    
+    return (
+        <li>
+            <Link {...props}
+                  className={`flex items-center py-2 px-5 text-base font-normal text-slate-900 rounded-lg hover:bg-slate-200 ${onPage && "bg-slate-200"}`} />
+        </li>
+    );
+}
+
+function Sidebar() {
+    const navigate = useNavigate();
+    const location = useLocation();
+    const [search, setSearch] = useState("");
+    const [isAdmin, setIsAdmin] = useState(false);
+
+    useEffect(() => {
+        const searchQuery = new URLSearchParams(location.search).get("search");
+        setSearch(searchQuery ?? "");
+    }, [location]);
+    
+    useQuery("checkAdmin", {
+        queryFn: () => {
+            return ccFetch("/api/User/Current", "GET", navigate);
+        },
+        onSuccess: async (data: Response) => {
+            const user: User = await data.json();
+            if (user.role == "Admin") {
+                setIsAdmin(true);
+            }
+        }
+    });
+
+    const logout = useCallback(async () => {
+        await fetch("/api/User/Logout", {
+            method: "POST"
+        });
+        navigate("/login");
+    }, [navigate]);
+
+    const onSearch = (event: React.KeyboardEvent<HTMLInputElement>) => {
+        // onSearch doesn't work on firefox, so this is the best we can do.
+        if (event.key === 'Enter') {
+            const target = event.target as HTMLInputElement;
+
+            if (target.value === 'enter') {
+                navigate('/movies');
+            } else {
+                navigate(`/movies?search=${target.value}`);
+            }
+        }
+    };
+    
+    return (
+        <ul className="basis-72">
+            <SidebarEntry to="/">Home</SidebarEntry>
+            <SidebarEntry to="/movies">Movies</SidebarEntry>
+            <SidebarEntry to="#" onClick={() => logout()}>Logout</SidebarEntry>
+
+            {isAdmin &&
+                <>
+                    <SidebarEntry to="/admin/users">Users</SidebarEntry>
+                    <SidebarEntry to="/admin/libraries">Libraries</SidebarEntry>
+                </>
+            }
+
+            <input type="search" placeholder="Search" onKeyUp={onSearch} defaultValue={search} />
+        </ul>
+    );
+}
+
+export default function Layout() {
+    return (
+        <div className="flex">
+            <Sidebar />
+
+            <div className="grow p-3">
+                <Outlet />
+            </div>
+        </div>
+    );
+}
